5b63f37bc0b8cb6edc3801a5e7f44d5488c66c4d
Caique Araujo <caique@studiopiggly.com.br>
2023-04-24 01:15:50 -0300

    :bug: fix(index): Export all data

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as helpers from './helpers';
 import * as www from './www';
 import * as core from './core';
 import * as utils from './utils';
+import * as types from './types';
 
-export { errors, helpers, www, core, utils };
+export { errors, helpers, www, core, utils, types };
