e40f023ab3bcd0352ff0c72861ef955657f50307
Caique Araujo <caique@studiopiggly.com.br>
2023-04-26 15:21:38 -0300

    :sparkles: feat(export): Collection of records

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export { default as BaseController } from './BaseController';
 export { default as BaseRecord } from './BaseRecord';
 export { default as BaseRequestBody } from './BaseRequestBody';
 export { default as ServiceManager } from './ServiceManager';
+export { default as CollectionOfRecord } from './CollectionOfRecord';
