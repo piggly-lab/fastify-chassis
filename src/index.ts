9e41366583dbc9ec8a5b6ac319f00111a25385a5
Caique Araujo <caique@studiopiggly.com.br>
2023-05-03 05:57:08 -0300

    :ambulance: critical(0.1.0-beta.9): Export entities and events

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import './lib/moduleAlias';
 export * from './errors';
 export * from './helpers';
 export * from './www';
+export * from './events';
+export * from './entities';
 export * from './core';
 export * from './utils';
 export * from './types';
