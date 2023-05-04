32b7cd7bad41b839eeabd324fed41451e4e99beb
Caique Araujo <caique@studiopiggly.com.br>
2023-05-03 22:54:59 -0300

    :package: refactor(aggregate): Remove collection for aggregate

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,5 @@
 export { default as AggregateRoot } from './AggregateRoot';
 export { default as BaseEntity } from './BaseEntity';
-export { default as CollectionOfAggregateRoot } from './CollectionOfAggregateRoot';
 export { default as CollectionOfEntity } from './CollectionOfEntity';
 export { default as EntityIdentifier } from './EntityIdentifier';
 export { default as EntityUUID } from './EntityUUID';
